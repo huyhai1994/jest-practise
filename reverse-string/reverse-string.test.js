6845dfd343423282f8d1778149c9c9b2f0a6cb65
thaole98 <bongsuri98@gmail.com>
2025-07-07 21:37:38 +0700

    commit reverse-string.test.js

--- reverse-string/reverse-string.test.js
+++ reverse-string/reverse-string.test.js
@@ -1,1 +1,23 @@
-// bai tap ve nha
+import {describe, expect, test} from '@jest/globals'
+
+function reString(row) {
+    /*base: điểm dừng của bài toán...*/
+    if (row === 0 || row === null || row === "" || isNaN(row) || typeof row === "boolean" ) {
+        throw new Error("input wrong!!!");
+    }
+    return row.split("").reverse().join("");
+}
+
+
+describe("test reverseString", () => {
+    test("input isNaN", () => {
+        expect(() => reString(isNaN)).toThrow(new Error("input wrong!!!"));
+    })
+    test("input boolean", () => {
+        expect(() => reString("boolean")).toThrow(new Error("input wrong!!!"));
+    })
+    test("input string", () => {
+        expect(reString("LETHAO")).toBe("OAHTEL");
+    })
+})
+
