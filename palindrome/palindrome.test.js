17eaaee195211ed03f918288e7c14d3156bf35b2
huyhai1994 <nguyenhuyhai926@gmail.com>
2025-07-09 22:24:16 +0700

    refactor: improve palindrome test cases and add new test file

--- palindrome/palindrome.test.js
+++ palindrome/palindrome.test.js
@@ -2,18 +2,22 @@ import {describe, expect, test} from '@jest/globals'
 
 // so sánh chữ trước khi đảo ngược và sau khi đảo ngược nếu, bằng nhau trả về true
 function isPalindrome(inputString) {
-    if (inputString==="" ||typeof inputString !== "string") {
+    if (inputString === "" || typeof inputString !== "string") {
         throw new Error("input wrong!!!");
     }
-    if (inputString.toUpperCase().split("").reverse().join("") === inputString.toUpperCase()) {
-        return true;
-    }
-    return false;
+    inputString = inputString.toLowerCase();
+    return inputString
+        .split("")
+        .reverse()
+        .join("") === inputString;
 }
+
+// [A B C B A]
+// [0,1,2,3,4]
 
 describe("test palindrome", () => {
     test("input ''", () => {
-        expect(()=>isPalindrome("")).toThrow(new Error("input wrong!!!"));
+        expect(() => isPalindrome("")).toThrow(new Error("input wrong!!!"));
     })
     test("input ABDBA", () => {
         expect(isPalindrome("ABDBA")).toBe(true);
@@ -28,10 +32,10 @@ describe("test palindrome", () => {
         expect(isPalindrome("Madam")).toBe(true);
     })
     test("input null", () => {
-        expect(()=>isPalindrome(null)).toThrow(new Error("input wrong!!!"));
+        expect(() => isPalindrome(null)).toThrow(new Error("input wrong!!!"));
     })
     test("input undefined", () => {
-        expect(()=>isPalindrome(undefined)).toThrow(new Error("input wrong!!!"));
+        expect(() => isPalindrome(undefined)).toThrow(new Error("input wrong!!!"));
     })
 })
 
---
+++ palindrome/palindrome.v2.test.js
@@ -0,0 +1,75 @@
+import {describe, expect, test} from '@jest/globals'
+
+describe("test palindrome", () => {
+    test("input ''", () => {
+        expect(() => isPalindrome("")).toThrow(new Error("input wrong!!!"));
+    })
+    test("input null", () => {
+        expect(() => isPalindrome(null)).toThrow(new Error("input wrong!!!"));
+    })
+    test("input undefined", () => {
+        expect(() => isPalindrome(undefined)).toThrow(new Error("input wrong!!!"));
+    })
+    test("input A", () => {
+        expect(isPalindrome("A")).toBe(true);
+    })
+    test("input AB", () => {
+        expect(isPalindrome("AB")).toBe(false);
+    })
+    test("input AA", () => {
+        expect(isPalindrome("AA")).toBe(true);
+    })
+    test("input ABA", () => {
+        expect(isPalindrome("ABA")).toBe(true);
+    })
+    test("input AAA", () => {
+        expect(isPalindrome("AAA")).toBe(true);
+    })
+    test("input AAa", () => {
+        expect(isPalindrome("AAa")).toBe(true);
+    })
+    test("input ABBA", () => {
+        expect(isPalindrome("ABBA")).toBe(true);
+    })
+    test("input ABCA", () => {
+        expect(isPalindrome("ABCA")).toBe(false);
+    })
+    test("input ABCBA", () => {
+        expect(isPalindrome("ABCBA")).toBe(true);
+    })
+    test("input ABCCA", () => {
+        expect(isPalindrome("ABCCA")).toBe(false);
+    })
+
+    test("input ABCCBA", () => {
+        expect(isPalindrome("ABCCBA")).toBe(true);
+    })
+    test("input ABCDBA", () => {
+        expect(isPalindrome("ABCDBA")).toBe(false);
+    })
+})
+
+function isPalindrome(inputString) {
+    if (inputString === "" || typeof inputString !== "string") {
+        throw new Error("input wrong!!!");
+    }
+    if (inputString.length === 1) {
+        return true;
+    }
+    inputString = inputString.toLowerCase();
+    if (inputString.length === 2) {
+        return inputString[0] === inputString[inputString.length - 1];
+    }
+    if (inputString.length > 5) {
+        return inputString[1] === inputString[4] && inputString[2] === inputString[3];
+    }
+    if (inputString.length > 4) {
+        return inputString[1] === inputString[3];
+    }
+    if (inputString.length > 3) {
+        return inputString[1] === inputString[2];
+    }
+    return true;
+}
+
+
